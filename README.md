4ee39db13054c3cc25823312ca2e302a28cfc477
usbpaul <usb.paul@gmail.com>
2014-04-05 14:30:07 +0200

    Code cleanup.

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Group members
 ## About
 
 This is a WordPress plugin that can expose a list of WordPress users through a shortcode.
+
+It relies on [Role Scoper](http://wordpress.org/plugins/role-scoper/) for categorization of users into groups.
--- group-members.php
+++ group-members.php
@@ -24,93 +24,35 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-/*
-Found the code for this at http://wordpress.org/support/topic/plugin-user-groups-list-group-members
- */
-
 function my_group_list_shortcode($attrs) {
 
     extract(shortcode_atts(
     	array(
-        'group' => 'No Group' // No Group is a defined user-group
+        'group' => 'No Group'
       ), $attrs
 		));
     global $wpdb;
 
     $taxonomy = 'user-group';
 
-    // Use a dBase query to get the ID of the user group
     	$query = "SELECT ug.user_id FROM wp_groups_rs as g, wp_user2group_rs as ug " .
 			"where g.group_name = '" .$group . "' AND ug.group_id = g.ID";
     
     $userIDs = $wpdb->get_results($query);
 
-    // Check if any user IDs were returned; if so, display!
-    // If not, notify visitor none were found.
     if ($userIDs) {
         $content = "<div class='group-list'><ul>";
-			// $content .= $userIDs;
         foreach( $userIDs as $userStdObj ) {
         	foreach ($userStdObj as $key => $userID) {
-            // $user = get_user_by('id', $userID);
-						// $content .= $user -> user_firstname;
             $user = get_user_by('id', $userID);
             $content .= "<li>";
             $content .= "" . $user->user_firstname . " " . $user->user_lastname . "";
-//            $content .= "<h3>" . $user->user_firstname . " " . $user->user_lastname . " <a href='mailto:" . $user -> user_email . "'>Mail</a></h3>";
             $content .= "</li>";
         	}
         }
         $content .= "</ul></div>";
     } else {
-        $content =
-        "<div class='group-list group-list-none'>Geen resultaten</div>";
-
-    }
-    return $content;
-}
-
-function niet_gebruikte_functie($add) {
-    // Dit impliceert dat er een taxonomie user-group wordt gemaakt. Kan ik nog proberen
-
-    // Get the global $wpdb object
-    global $wpdb;
-
-    // Extract the parameters and set the default
-    extract ( shortcode_atts( array(
-        'group' => 'No Group' // No Group is a defined user-group
-        ), $atts ) );
-
-    // The taxonomy name will be used to get the objects assigned to that group
-    $taxonomy = 'user-group';
-
-    // Use a dBase query to get the ID of the user group
-    $userGroupID = $wpdb->get_var(
-                    $wpdb->prepare("SELECT term_id
-                        FROM {$wpdb->terms} t
-                        WHERE t.name = %s", $group));
-
-    // Now grab the object IDs (aka user IDs) associated with the user-group
-    $userIDs = get_objects_in_term($userGroupID, $taxonomy);
-
-    // Check if any user IDs were returned; if so, display!
-    // If not, notify visitor none were found.
-    if ($userIDs) {
-        $content = "<div class='group-list'> <ul>";
-        foreach( $userIDs as $userID ) {
-            $user = get_user_by('id', $userID);
-            $content .= "<li>";
-            $content .= get_avatar( $user->ID, 70 );
-            $content .= "<h3>" . $user->display_name . "</h3>";
-            $content .= "<p><a href='". get_author_posts_url( $user->ID ) . "' class='more-info-icon'>More info</a>";
-            $content .= "<!-- add more here --></p>";
-            $content .= "</li>";
-        }
-        $content .= "</ul></div>";
-    } else {
-        $content =
-        "<div class='group-list group-list-none'>Returned no results</div>";
-
+        $content = "<div class='group-list group-list-none'>Geen leden gevonden</div>";
     }
     return $content;
 }
